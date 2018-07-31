6dd039d3cf780d1c6964205e1e3192f8bcd5fb43
jonodeno <jondenose@gmail.com>
2018-07-31 15:07:53 -0500

    logic is correct, images need to change
    correct text is updated
    clicking works
    need to change img src

--- CatClicker.js
+++ CatClicker.js
@@ -1,27 +1,61 @@
-var button1 = document.getElementById("btn1");
-var button2 = document.getElementById("btn2");
+//let button = document.getElementById("btn");
+let list = document.getElementById("list-container");
+let clickTimes = document.getElementById("timesClicked");
+clickTimes.innerHTML = "0";
+let catName = document.getElementById("name");
+const buttonDiv = document.getElementById("button-div");
+let prevCatEnt = 0;
 
-var clickNum1 = 0;
-var clickNum2 = 0;
-
-var display1 = document.getElementById("timesClicked1");
-var display2 = document.getElementById("timesClicked2");
-
-var name1 = document.getElementById("kitty1Name");
-name1.innerHTML = "Kitty 1";
-var name2 = document.getElementById("kitty2Name");
-name2.innerHTML = "Kitty 2";
-
-if(button1){
-  button1.addEventListener('click',function(){
-    clickNum1+=1;
-    display1.innerHTML=clickNum1;
-  },false);
+function Cat(num){
+  this.catNumber = num;
+  //create element for name to be displayd when cat name clicked
+  this.kittyName = "Kitty "+num;
+  this.timesClicked = 0;
 }
 
-if(button2){
-  button2.addEventListener('click',function(){
-    clickNum2+=1;
-    display2.innerHTML=clickNum2;
-  },false);
+const numCats = [1,2,3,4,5];
+let cats = [];
+let buttons = [];
+let imgs = [];
+
+function setCat(c,num){
+  catName.innerHTML = c.kittyName;
+  //button.innerHTML = "Click for Kitty "+c.catNumber+"!";
+  clickTimes.innerHTML=c.timesClicked;
+  buttons[prevCatEnt].style.display = "none";
+  buttons[num].style.display = "block";
+  return num;
 }
+
+function createListEnt(num){
+  let entry = document.createElement("BUTTON");
+  let text = document.createTextNode("Kitty"+num);
+  entry.appendChild(text);
+  entry.addEventListener('click',function(){
+    prevCatEnt = setCat(cats[num-1],num-1);
+  },false)
+  list.appendChild(entry);
+}
+
+//initialize the list
+for (let i = 1; i < numCats.length+1; i++) {
+  let tempCat = new Cat(i);
+  cats.push(tempCat);
+  //create the button
+  let entry = document.createElement("BUTTON");
+  let text = document.createTextNode("Click for Kitty "+i+"!");
+  entry.appendChild(text);
+  entry.addEventListener('click',function(){
+    //MAY NEED TO CHANGE TO I-1
+    cats[i-1].timesClicked++;
+    clickTimes.innerHTML=cats[i-1].timesClicked;
+  },false)
+  entry.style.display = "none";
+  buttonDiv.appendChild(entry);
+  buttons.push(entry);
+  //create the list entry
+  createListEnt(i);
+  //create img entry
+  //img.push("<img src='img/cat"+i+".jpg'");
+}
+prevCatEnt = setCat(cats[0],0);
