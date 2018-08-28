51b4e4480fc8d4d36c11a08fb66b09162aa5a9a9
jonodeno <jondenose@gmail.com>
2018-08-28 12:43:06 -0500

    finished mvo refactoring

--- CatClicker.js
+++ CatClicker.js
@@ -1,43 +1,46 @@
-
 let prevCatEnt = 0;
 const totalCats = 5;
 
 function Cat(num,url){
-  this.catNumber = num;
   //create element for name to be displayd when cat name clicked
-  this.kittyName = "Kitty "+num;
+  this.kittyName = "Kitty "+(num+1);
   this.timesClicked = 0;
   this.imgURL=url;
 }
 
 //model, only holds data
 let model = {
-  lastId: 0,
   cats: []
 };
 
 //octopus, interface between data and view
 const octopus = {
   //function to change to the newest cat
-  setCat: function(c,num){
-    catName.innerHTML = c.kittyName;
-    clickTimes.innerHTML=c.timesClicked;
-    buttons[prevCatEnt].style.display = "none";
-    buttons[num].style.display = "block";
-    return num;
+  setCat: function(num){
+    return function(){
+      console.log(prevCatEnt);
+      catView.render(num);
+    }
   },
   //increments a cat's number of times clicked
-  incCat: function(catNum){
-    model.cats[catNum].timesClicked++;
+  incCat: function(i){
+    return function(){
+      model.cats[i].timesClicked++;
+      catView.render(i);
+      //catView.updateTimesClicked(model.cats[i].timesClicked);
+    }
   },
   init: function(){
     //initialize array of cats
-    for (var i = 1; i <= totalCats; i++) {
-      model.cats.push(new Cat(i,"img/cat"+i+".jpg"))
+    for (var i = 0; i < totalCats; i++) {
+      model.cats.push(new Cat(i,"img/cat"+(i+1)+".jpg"))
     };
+    console.log(model.cats);
 
     //initialize list of buttons
     listView.init();
+    //initialize cat img area
+    catView.init();
   }
 };
 
@@ -49,17 +52,15 @@ let listView = {
   //creates a button for each cat in the list of cats
   render: function (num){
     let entry = document.createElement("BUTTON");
-    let text = document.createTextNode("Kitty"+num);
+    let text = document.createTextNode("Kitty"+(num+1));
     entry.appendChild(text);
-    entry.addEventListener('click',function(){
-      prevCatEnt = octopus.setCat(cats[num-1],num-1);
-    },false)
+    entry.addEventListener('click',octopus.setCat(num),false)
     this.list.appendChild(entry);
   },
 
   //initialize list of buttons
   init: function(){
-    for (var i = 1; i <= totalCats; i++) {
+    for (var i = 0; i < totalCats; i++) {
       this.render(i);
     }
   }
@@ -68,11 +69,42 @@ let listView = {
 //view for cat image area
 let catView = {
   //html
-  this.img = document.getElementById("cat-img");
-  this.clickTimes = document.getElementById("timesClicked");
-  clickTimes.innerHTML = "0";
-  this.catName = document.getElementById("name");
-  this.buttonDiv = document.getElementById("button-div");
+  img: document.getElementById("cat-img"),
+  clickTimes: document.getElementById("timesClicked"),
+  catName: document.getElementById("name"),
+  buttonDiv: document.getElementById("button-div"),
+
+  //properties
+  buttons: [],
+
+  render: function(num){
+    let c = model.cats[num];
+    this.img.src = c.imgURL;
+    this.catName.innerHTML = c.kittyName;
+    this.clickTimes.innerHTML=c.timesClicked;
+    this.buttons[prevCatEnt].style.display = "none";
+    this.buttons[num].style.display = "block";
+    prevCatEnt = num;
+  },
+  updateTimesClicked: function(num){
+    this.clickTimes.innerHTML = num;
+  },
+  init: function(){
+    //make buttons
+    for (var i = 0; i < totalCats; i++) {
+      let entry = document.createElement("BUTTON");
+      let text = document.createTextNode("Click for Kitty "+(i+1)+"!");
+      entry.appendChild(text);
+      entry.addEventListener('click',octopus.incCat(i),false)
+      entry.style.display = "none";
+      this.buttonDiv.appendChild(entry);
+      this.buttons.push(entry);
+      console.log(this.buttons);
+    }
+
+    this.clickTimes.innerHTML = "0";
+    this.render(0);
+  }
 }
 
 octopus.init();
